7f5e245985e026c29fa1714629614e28037218ce
Ahmed Aly <ahmealy.18@gmail.com>
2021-11-01 12:19:19 +0200

    Backend System for a car website

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO price (currency, price, vehicle_id) VALUES ('dollar', 22, 1);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('dollar', 35, 2);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('dollar', 60, 3);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('Euro', 10, 4);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('Euro', 20, 5);
+INSERT INTO price (currency, price, vehicle_id) VALUES ('Euro', 30, 6);
